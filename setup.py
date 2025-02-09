e0e2f4ae2da2f74254f4312dea53a08a7e50cfa8
Bernard Ngangbam <bernardng699@gmail.com>
2025-02-09 14:01:14 +0900

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Bernard Ngangbam',
+    author_email= 'bernardng699@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
